fabb810ace69328171c8bc864e95e5745d3b51ba
Tabishahmad <tabishahmad1@gmail.com>
2023-10-25 15:19:53 +0530

    11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.samplelibrary"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.samplelibrary"
